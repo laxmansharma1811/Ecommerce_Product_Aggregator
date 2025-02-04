fb2e0a9aefe4f2700542ba04a73ddfcd7c993664
Laxman Sharma <laxman.sharma@gmail.com.com>
2025-02-04 22:19:52 +0545

    Configuration

--- product_aggregator/settings.py
+++ product_aggregator/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'product_aggregator.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Add these settings to your settings.py file
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -104,12 +104,12 @@ EMAIL_HOST_PASSWORD = 'Add your password'
 # }
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "mydatabase",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": "mydatabase",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
