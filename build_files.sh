d888a025dfddbcbadf58acb7cbddd7e4d1f1e15a
Laxman Sharma <laxman.sharma@gmail.com.com>
2025-02-04 21:59:27 +0545

    Configuration

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
+
+# Ensure Python and pip are installed
+apt update && apt install -y python3 python3-pip
+
+# Install dependencies
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+
+# Run migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
